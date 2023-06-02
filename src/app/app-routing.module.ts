10f200338efe3f1b6a8b4ea7296f1edd6766d721
Ashley Grevelink <ashleyg@web3.foundation>
2023-06-02 17:30:53 -0400

    add new route for login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 
+
+// promise .then means its not loaded but lazy loaded user module (see in networking tab)
 const routes: Routes = [
-  { path: '', component: HomePageComponent }
+  { path: '', component: HomePageComponent },
+  {
+    path: 'login', loadChildren: () => import('./user/user.module').then(m => m.UserModule)
+  }
 ];
+
+
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
