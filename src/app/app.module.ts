1f6919987cfa39aa8eba61652d9a23811b083d46
Ashley Grevelink <ashleyg@web3.foundation>
2023-06-02 17:12:43 -0400

    firebase database config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,33 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { SharedModule } from './shared/shared.module';
+import { HomePageComponent } from './home-page/home-page.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideFunctions,getFunctions } from '@angular/fire/functions';
+import { providePerformance,getPerformance } from '@angular/fire/performance';
+import { provideStorage,getStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    SharedModule
+    SharedModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore()),
+    provideFunctions(() => getFunctions()),
+    providePerformance(() => getPerformance()),
+    provideStorage(() => getStorage())
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'nanoidentitycontracts',
+    appId: '1:492486535675:web:cac08347059ffc6652835e',
+    storageBucket: 'nanoidentitycontracts.appspot.com',
+    apiKey: 'AIzaSyBXBZmABVobkQibnEFKT_zGABry_591SIA',
+    authDomain: 'nanoidentitycontracts.firebaseapp.com',
+    messagingSenderId: '492486535675',
+  },
+};
